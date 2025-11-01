645c60063fef0d752f6fb1cf78b397176597c94c
ErickGuerron <erickguerron@yahoo.com>
2025-11-01 00:10:31 -0500

    feat: tailwind funcional y concepto de home

--- sistemaFacturacion/tailwind.config.js
+++ sistemaFacturacion/tailwind.config.js
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
+
     content: [
-        "./**/*.{razor,html,cshtml}" // Esto le dice a Tailwind que mire todos tus archivos Blazor
+        "./**/*.{razor,html,cshtml,css}" 
     ],
     theme: {
-        extend: {},
+        extend: {
+            
+            keyframes: {
+                fadeIn: {
+                    '0%': { opacity: '0' },
+                    '100%': { opacity: '1' },
+                },
+                slideInUp: {
+                    '0%': { opacity: '0', transform: 'translateY(20px)' },
+                    '100%': { opacity: '1', transform: 'translateY(0)' },
+                },
+                slideInRight: {
+                    '0%': { transform: 'translateX(100%)', opacity: '0' },
+                    '100%': { transform: 'translateX(0)', opacity: '1' },
+                }
+            },
+            
+            animation: {
+                fadeIn: 'fadeIn 0.3s ease-out forwards',
+                slideInUp: 'slideInUp 0.5s ease-out forwards',
+                slideInRight: 'slideInRight 0.3s ease-out forwards',
+            },
+        },
     },
     plugins: [],
 }
